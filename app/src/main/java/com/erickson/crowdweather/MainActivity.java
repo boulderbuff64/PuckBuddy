4df804f74f4de009cc022848f6e863b1c0eaccb6
Andrew Erickson <Andrew.Erickson@MJDinteractive.com>
2016-08-01 19:43:56 -0400

    Working touch and displaying circles

--- app/src/main/java/com/erickson/crowdweather/MainActivity.java
+++ app/src/main/java/com/erickson/crowdweather/MainActivity.java
@@ -1,45 +1,47 @@
 package com.erickson.crowdweather;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
-import org.w3c.dom.Text;
+//import android.support.v4.view.GestureDetectorCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements GestureDetector.OnGestureListener, OnClickListener {
 
     private Firebase mRef;
     private Firebase mRefTemp;
+    PaintSurface mPaintSurface;
+    Draw drawView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Firebase.setAndroidContext(this);
+        Log.d("onCreate()", "1");
+
+        mPaintSurface = (PaintSurface) findViewById(R.id.paint_surface); // Getting reference to the PaintView R.layout.activity_main
+        mPaintSurface.setOnTouchListener(mPaintSurface);
+        drawView = new Draw(this);
+        drawView.setBackgroundColor(Color.BLACK);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Button mButtonFoggy = (Button) findViewById(R.id.buttonFoggy);
-        Button mButtonSunny = (Button) findViewById(R.id.buttonSunny);
-        TextView mTempText = (EditText) findViewById(R.id.TempText);
-        final TextView mTextCondition = (TextView) findViewById(R.id.textViewCondition);
-
         //mRef = new Firebase("http://androidqs-firebaseio-demo.com/condition");
         mRef = new Firebase("https://burning-inferno-8506.firebaseio.com/condition");
         mRefTemp = new Firebase("https://burning-inferno-8506.firebaseio.com/temp");
@@ -49,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 String newCondition = (String) dataSnapshot.getValue();
-                mTextCondition.setText(newCondition);
+                //mTextCondition.setText(newCondition);
             }
 
             @Override
@@ -57,21 +59,12 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
-        mButtonSunny.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                mRef.setValue("Sunny");
-            }
-        });
-
-        mButtonFoggy.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                mRef.setValue("Foggy");
-            }
-        });
-
+    protected void onResume() {
+        super.onResume();
+        mPaintSurface.resume();
+        super.onResume();
     }
 
     @Override
@@ -94,5 +87,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
---
+++ app/src/main/java/com/erickson/crowdweather/Puck.java
@@ -0,0 +1,312 @@
+package com.erickson.crowdweather;
+/**
+ * Created by ASErickson on 5/26/16.
+ */
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+
+public class Puck {
+    float x;
+    float y;
+    float x2;
+    float y2;
+    float Vx;
+    float Vy;
+    float Vx2;
+    float Vy2;
+    int width;
+    int height;
+    int edge;
+    int rad;
+    int VeloScalar = 30;
+    float theta = 0;
+    float theta2 = (float) 1.57;
+    static float Pi = (float) 3.14159;
+    android.graphics.Paint Paint = new Paint();
+
+    public void start(int Width, int Height, int Radius) {
+        Log.d("Puck", "start");
+        width = Width;
+        height = Height;
+        edge = 0; //(int) (Width * 0.02);
+        rad = Radius;
+        x = (float) (Math.random() * Width);
+        y = (float) (Math.random() * Height);
+        x2 = width / 2;
+        y2 = height / 2;
+        Vx = 0;
+        Vy = 0;
+        Vx2 = 0;
+        Vy2 = 0;
+        theta = (float) (Math.random() * 2 * Pi);
+        Paint.setStrokeWidth(5);
+    }
+
+    public void setColor(int R, int G, int B) {
+        Paint.setColor(Color.rgb(R, G, B));
+    }
+
+    public void setRandomColor() {
+        Paint.setColor(Color.rgb((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+    }
+
+    public void setRandomVelo(float d) {
+        Vx = (float) (d * (2 * Math.random() - 1));
+        Vy = (float) (d * (2 * Math.random() - 1));
+    }
+
+    public void setRandomLocation() {
+        x = (float) Math.random() * width;
+        y = (float) Math.random() * height;
+    }
+
+    public void setRainbowColor(float A, float B) {
+        setColor((int) (Math.sin(-1.5 + Pi * (A * x / width + B * y / height)) * 128 + 128),
+                (int) (Math.sin(0.6 + Pi * (A * x / width + B * y / height)) * 128 + 128),
+                (int) (Math.sin(2.7 + Pi * (A * x / width + B * y / height)) * 128 + 128));
+    }
+
+    public void addSpeed() {
+        //Log.d("Puck", "addSpeed");
+        x = x + Vx; //add X velocity
+        y = y + Vy; //add Y velocity
+        x2 = x2 + Vx2; //line
+        y2 = y2 + Vy2; //line
+
+    }
+
+    public void edgeBounce(boolean bounce) {
+        //Log.d("Puck", "edgeBounce");
+        if (bounce) {
+            if (x > width - edge - rad) {
+                Vx = Vx * -1;
+                x = width - edge - rad;
+            }
+            if (x < 0 + edge + rad) {
+                Vx = Vx * -1;
+                x = edge + rad;
+            }
+            if (y > height - edge - rad) {
+                Vy = Vy * -1;
+                y = height - edge - rad;
+            }
+            if (y < edge + rad) {
+                Vy = Vy * -1;
+                y = edge + rad;
+            }
+        } else {
+            if (x >= width) {
+                x = x - width;
+            }
+            if (x <= 0) {
+                x = x + width;
+            }
+            if (y >= height) {
+                y = y - height;
+            }
+            if (y <= 0) {
+                y = y + height;
+            }
+        }
+    }
+
+    public boolean near(float X, float Y, int RAD) {
+        boolean B = false;
+        double dist = Math.sqrt((X - x) * (X - x) + (Y - y) * (Y - y));
+        if (dist < (rad + RAD)) {
+            B = true;
+        }
+        return B;
+    }
+
+    public void checkPaddleBounce(float Xh, float Yh, float Vxh, float Vyh, int Radius) {
+        float dist = scalar((Xh - x), (Yh - y));
+        if (dist < Radius + rad & Radius > 0) {
+            // print(" Vx=",Vxp," Vy=",Vyp);
+            float[] temp = bounce(Vx, Vy, (Xh - x), (Yh - y));
+            Vx = temp[0] + Vxh / VeloScalar;
+            Vy = temp[1] + Vyh / VeloScalar;
+            //Log.d("Vh= ", Vxh / VeloScalar + "  " + Vyh / VeloScalar);
+            //canvas.drawLine(Xp, Yp, Xp + Vxp * 100, Yp + Vyp * 100, playerPaint); // line(Xp,Yp,Xp+Vxp*40,Yp+Vyp*40);
+
+            x = x + Vx;
+            y = y + Vy;
+            dist = scalar((Xh - x), (Yh - y));
+            while (dist < Radius + rad) {
+                dist = scalar((Xh - x), (Yh - y));
+                x = x + Vx;
+                y = y + Vy;
+            }
+        }
+    }
+
+    private static float scalar(float A, float B) {
+        return (float) Math.sqrt(A * A + B * B);
+    }
+
+    public static float[] bounce(float Xb, float Yb, float Xs, float Ys) {
+        float Norm, In;
+        float Pi = (float) 3.141529;
+        if (Xs > 0) {
+            Norm = (float) (Math.atan(Ys / (Xs + 0.01)) + Pi / 2);
+        } else {
+            Norm = (float) (Math.atan(Ys / (Xs - 0.01)) + Pi * 3 / 2);
+        }
+        if (Xb > 0) {
+            In = (float) Math.atan(Yb / (Xb + 0.01));
+        } else {
+            In = (float) (Math.atan(Yb / (Xb - 0.01)) + Pi);
+        }
+        float Bounce = Norm * 2 - In;
+
+        float[] New = {0, 0};
+        New[0] = (float) (Math.cos(Bounce) * scalar(Xb, Yb));
+        New[1] = (float) (Math.sin(Bounce) * scalar(Xb, Yb));
+        // System.out.println(" New Vx=" + New[0] + " New Vy=" + New[1] + " surf=" + Math.atan(Ys / Xs) + " Norm=" + Norm + " In=" + In + " B=" +
+        // Bounce);
+        return New;
+    }
+
+    public void speedLimit(float maxV, float dragV) {
+        float VeloMag = scalar(Vx, Vy);
+        if (VeloMag > maxV) {
+            Vx = Vx / VeloMag * maxV;
+            Vy = Vy / VeloMag * maxV;
+        }
+        if (VeloMag > dragV) {
+            Vx = (float) (Vx * 0.996);
+            Vy = (float) (Vy * 0.996);
+            // System.out.println(" Vxp=" + Vxp + " Vyp=" + Vyp);
+        }
+    }
+
+    public void Gravity(float X, float Y, float Power) {
+        //Log.d("Puck", "Gravity");
+        float Dist;
+        float accel;
+        Dist = (float) (Math.sqrt((x - X) * (x - X) + (y - Y) * (y - Y)) + 0.001) / 2;
+        if (Dist > width / 100) {
+            accel = 1 / Dist / Dist; //accelerate to finger
+        } else {
+            accel = 0; // if close, don't accelerate
+        }
+
+        Vx += Power * accel * Math.cos(Math.atan((y - Y) / (x - X + 0.001))) * -(x - X) / Math.abs((x - X + 0.001));
+        Vy += Power * accel * Math.sin(Math.atan((y - Y) / (x - X + 0.001))) * -(x - X) / Math.abs((x - X + 0.001));
+    }
+
+    public void Sink(float Y, float Power) {
+        //Log.d("Puck", "Sink");
+        if (y < Y) {
+            Vy += Power / 100;
+        } else {
+            Vy -= Power / 100;
+        }
+    }
+
+    public void Ellipse(float Fx, float Fy, float Gx, float Gy, float power, double dT) {
+        //Log.d("Puck", "Ellipse");
+        theta = (float) ((theta + dT) % (2 * Pi));
+        float shiftX, shiftY;
+        //if (Fx<Gx){ shiftX=Fx; } else {shiftX=Gx;}
+        //if (Fy<Gy){ shiftY=Fy; } else {shiftY=Gy;}
+        shiftX = (Fx + Gx) / 2;
+        shiftY = (Fy + Gy) / 2;
+        float X = (float) (Math.abs((Fx - Gx)) / 2 * Math.cos(theta)) + shiftX;
+        float Y = (float) (Math.abs((Fy - Gy)) / 2 * Math.sin(theta)) + shiftY;
+
+        Follow(X, Y, power);
+    }
+
+    public void Ellipse2(float Fx, float Fy, float Gx, float Gy, float power, double dT) {
+        //Log.d("Puck", "Ellipse2");
+        theta2 = (float) ((theta2 + dT) % (2 * Pi));
+        float shiftX, shiftY;
+        //if (Fx<Gx){ shiftX=Fx; } else {shiftX=Gx;}
+        //if (Fy<Gy){ shiftY=Fy; } else {shiftY=Gy;}
+        shiftX = (Fx + Gx) / 2;
+        shiftY = (Fy + Gy) / 2;
+        float X = (float) (Math.abs((Fx - Gx)) / 2 * Math.cos(theta2)) + shiftX;
+        float Y = (float) (Math.abs((Fy - Gy)) / 2 * Math.sin(theta2)) + shiftY;
+
+        double thta = 0;
+        float dist = scalar(X - x2, Y - y2);
+        float dX = X - x2;
+        float dY = Y - y2;
+
+        if (dist >= power) {
+            //Vx = (float) (Power * Math.sin((X - x) / dist));
+            //Vy = (float) ((Y - y) / Math.abs(Y - y) * Power * Math.cos((Y - y) / dist));
+
+            thta = Math.atan(dX / dY);
+            Vx2 = (float) (sign(dY) * power * Math.sin(thta) + Vx2) / 2;
+            Vy2 = (float) (sign(dY) * power * Math.cos(thta) + Vy2) / 2;
+        } else {
+            Vx2 = dX;
+            Vy2 = dY;
+        }
+    }
+
+    public void ForceMove(float A, float B, float X, float Y, float Power) {
+        //Log.d("Puck", "ForceMove");
+
+        float Dist1, Dist2;
+        float accel1, accel2;
+        Dist1 = (float) (Math.sqrt((x - X) * (x - X) + (y - Y) * (y - Y)) + 1);
+        Dist2 = (float) (Math.sqrt((x - A) * (x - A) + (y - B) * (y - B)) + 1);
+        accel1 = 1 / Dist1;
+        accel2 = 1 / Dist2;
+
+        if (Dist1 > Math.sqrt(Power)) {
+            accel1 = accel1 / Dist1; //Squared
+        }
+        if (Dist2 > Math.sqrt(Power)) {
+            accel2 = accel2 / Dist2; //Squared
+        }
+
+        Vx += Power * accel2 * Math.cos(Math.atan((y - Y) / (x - X + 0.001))) * -(x - X) / Math.abs((x - X + 0.001));
+        Vy += Power * accel2 * Math.sin(Math.atan((y - Y) / (x - X + 0.001))) * -(x - X) / Math.abs((x - X + 0.001));
+        Vx += Power * accel1 * Math.cos(Math.atan((y - B) / (x - A + 0.001))) * -(x - A) / Math.abs((x - A + 0.001));
+        Vy += Power * accel1 * Math.sin(Math.atan((y - B) / (x - A + 0.001))) * -(x - A) / Math.abs((x - A + 0.001));
+    }
+
+    public void stuckInCorner() {
+        if (x < 5 & y < 5) {
+            x = width / 2;
+            y = height / 2;
+        }
+        if (x > width & y > height) {
+            x = width / 2;
+            y = height / 2;
+        }
+    }
+
+    public void Follow(float X, float Y, float Power) {
+        //Log.d("Puck", "Follow");
+
+        double thta = 0;
+        float dist = scalar(X - x, Y - y);
+        float dX = X - x;
+        float dY = Y - y;
+
+        if (dist >= Power) {
+            //Vx = (float) (Power * Math.sin((X - x) / dist));
+            //Vy = (float) ((Y - y) / Math.abs(Y - y) * Power * Math.cos((Y - y) / dist));
+
+            thta = Math.atan(dX / dY);
+            Vx = (float) (sign(dY) * Power * Math.sin(thta) + Vx) / 2;
+            Vy = (float) (sign(dY) * Power * Math.cos(thta) + Vy) / 2;
+        } else {
+            Vx = dX;
+            Vy = dY;
+        }
+
+    }
+
+    private float sign(float A) {
+        return (float) (A / Math.abs(A));
+    }
+
+}
